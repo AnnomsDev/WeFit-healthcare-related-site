e4e3cd589fe62d4db384ca7e1db70a5c64c721e7
AnnomsDev <foysalbinnour@gmail.com>
2021-10-18 18:21:29 +0600

    bassic header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header/Header';
+import Home from './components/Header/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
-      
-      
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
+
+
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import './Header.css';
+import logo from '../../img/log.svg';
+import { NavLink, Link } from 'react-router-dom';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light Header">
+            <div className="container-fluid">
+                <span className='logo'>
+                    <Link className="navbar-brand" to='/'><img height='50px' src={logo} alt="" /> </Link>
+                </span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to='/rd'>Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to='/rd'>Contact Us</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to='/rd'><span className='wf-btn-secondary'>Sign In</span></NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to='/rd'><span className='wf-btn-primary'>Sign Up</span></NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
